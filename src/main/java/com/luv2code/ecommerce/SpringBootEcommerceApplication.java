f1028b6c4cb75c348fbf1eeb17f1ab8ba571d1d2
fpankretic <filip.bonbon@gmail.com>
2023-11-16 21:06:02 +0100

    changes

--- src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
+++ src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootEcommerceApplication {
 
 	public static void main(String[] args) {
+		// This is only for PR
 		SpringApplication.run(SpringBootEcommerceApplication.class, args);
 	}
 
